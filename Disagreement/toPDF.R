67e537ee1b29028bc090a53de45b79cbf24a9489
Weitao <431259878@qq.com>
2023-10-30 01:45:30 +0800

    更改字体和主题颜色

--- Disagreement/toPDF.R
+++ Disagreement/toPDF.R
@@ -1,2 +1,3 @@
 library(renderthis)
-to_pdf(from = "C:\Users\User\Desktop\Presentation1101\Disagreement\Disagreement.html")
+to_pdf(from = "C:\\Users\\User\\Desktop\\Presentation1101\\Disagreement\\Disagreement.html")
+# to_pdf(from = "C:\\Users\\User\\Desktop\\Disagreement.html")
